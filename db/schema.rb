4aa44213e73e2719e7d3cb53d0eccbdbf67edc9e
Peter Jang <peter.x.jang@gmail.com>
2016-03-23 18:18:01 -0500

    created group model with has many through association with contact model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320192451) do
+ActiveRecord::Schema.define(version: 20160323230841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contact_groups", force: :cascade do |t|
+    t.integer  "contact_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string   "first_name"
@@ -26,6 +33,12 @@ ActiveRecord::Schema.define(version: 20160320192451) do
     t.float    "latitude"
     t.float    "longitude"
     t.integer  "user_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
